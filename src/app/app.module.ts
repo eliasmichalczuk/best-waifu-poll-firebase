e7db3d063a68e2a978cd2ad1ab04e19f0a4d4fe9
Elias Michalczuk <silva.elias@totvs.com.br>
2019-07-21 20:29:12 -0300

    patch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,34 +8,36 @@ import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BestWaifuPollComponent } from './best-waifu-poll/best-waifu-poll.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   declarations: [
-    BestWaifuPollComponent
+    BestWaifuPollComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig, 'angularfs'),
-    AngularFirestoreModule,
-    AngularFireDatabaseModule,
-    NgxSpinnerModule,
+    // AngularFireModule.initializeApp(environment.firebaseConfig, 'angularfs'),
+    // AngularFirestoreModule,
+    // AngularFireDatabaseModule,
+    // NgxSpinnerModule,
     BrowserAnimationsModule
   ],
   entryComponents: [
-    BestWaifuPollComponent
+    // BestWaifuPollComponent
   ],
   providers: [],
-  bootstrap: [BestWaifuPollComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 
-  constructor(private injector: Injector) {
-  }
+  // constructor(private injector: Injector) {
+  // }
 
-  ngDoBootstrap() {
-    const el = createCustomElement(BestWaifuPollComponent,
-      { injector: this.injector});
+  // ngDoBootstrap() {
+  //   const el = createCustomElement(BestWaifuPollComponent,
+  //     { injector: this.injector});
 
-    customElements.define('best-waifu-pool', el);
-  }
+  //   customElements.define('best-waifu-pool', el);
+  // }
 }
